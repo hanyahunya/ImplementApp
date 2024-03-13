9fbce01e5a2cc04fef301710e4b8d5c8f1bbba53
Jung Hamin <java020103@gmail.com>
2024-03-13 11:31:51 +0900

    Test

---
+++ Project1/Sample0313-1.c
@@ -0,0 +1,30 @@
+#include <stdio.h> // std(Standard 표준) io (input output) 스탠다드io라 읽음
+
+void main() //void 되돌려줄 값이 없는 메소드 리턴값이 있으면 마지막에 return값 줘야함
+{
+	/* 
+	int result; // <- 메모리 잡아먹는얘Integer
+	result = 100 - 50;
+	printf("%d", result); // %d 정수출력 (10진법) %c 문자출력 (Character) %s 문자출력 (String)
+	printf("%d", 100 - 50);
+	printf("%d",
+		100 - 50);
+	*/
+
+	int a, b;
+	int result;
+	a = 100;
+	b = 50;
+
+	result = a + b;
+	printf("%d + %d = %d \n", a, b, result);
+
+	result = a - b;
+	printf("%d - %d = %d \n", a, b, result);
+
+	result = a * b;
+	printf("%d * %d = %d \n", a, b, result);
+
+	result = a / b;
+	printf("%d / %d = %d \n", a, b, result);
+}
